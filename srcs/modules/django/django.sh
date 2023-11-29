89c13192a9f2b05c51645c575aeaa6ed33dbc612
mjuin <mickael.juin.16@gmail.com>
2023-11-29 12:59:26 +0100

    Empty sh

--- srcs/modules/django/django.sh
+++ srcs/modules/django/django.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -d mysite ]; then
-	echo "launch django-admin startproject mysite;";
-	django-admin startproject mysite;
-fi
+# if [ ! -d mysite ]; then
+	# echo "launch django-admin startproject mysite;";
+	# django-admin startproject mysite;
+# fi
 
-export IP_DJANGO=$(hostname -i);
-echo $IP_DJANGO > /var/db/ip_django
+# export IP_DJANGO=$(hostname -i);
+# echo $IP_DJANGO > /var/db/ip_django
 
 #export DB_NAME="transcendence_db"
 #export DB_USER="user_db"
@@ -28,51 +28,52 @@ while [ ! -f /var/db/check ] ; do
 done
 rm -rf /var/db/check
 
-export APP_NAME="transcendence";
+# export APP_NAME="transcendence";
 
 if [ ! -d "$APP_NAME/" ] ; then
-	python manage.py startapp $APP_NAME;
-
-	mkdir -p $APP_NAME/templates/$APP_NAME;
-	mkdir -p $APP_NAME/static/$APP_NAME;
-	mkdir -p $APP_NAME/templates/$APP_NAME/files;
-
-	cat ../conf/settings.py > mysite/settings.py;
-	ln -s /var/site_files/html/index.html $APP_NAME/templates/$APP_NAME/index.html
-	ln -s /var/site_files/css/ $APP_NAME/static/$APP_NAME/
-	ln -s /var/site_files/html/ $APP_NAME/templates/$APP_NAME/files;
-	ln -s /var/site_files/js/ $APP_NAME/static/$APP_NAME/;
-	ln -s /var/site_files/assets/ $APP_NAME/static/$APP_NAME/;
-	# mv ../site_files/html/index.html $APP_NAME/templates/$APP_NAME;
-	# mv ../site_files/css/ $APP_NAME/static/$APP_NAME/
-	# mv ../site_files/html/ $APP_NAME/templates/$APP_NAME/files;
-	# mv ../site_files/js/ $APP_NAME/static/$APP_NAME/js;
-	# mv ../site_files/assets/ $APP_NAME/static/$APP_NAME/;
-	cat ../conf/models.py > $APP_NAME/models.py;
-	cat ../conf/views.py > $APP_NAME/views.py;
-	cat ../conf/urls_app.py > $APP_NAME/urls.py;
-	cat ../conf/urls_project.py > mysite/urls.py;
+	# python manage.py startapp $APP_NAME;
+# 
+	# mkdir -p $APP_NAME/templates/$APP_NAME;
+	# mkdir -p $APP_NAME/static/$APP_NAME;
+	# mkdir -p $APP_NAME/templates/$APP_NAME/files;
+# 
+	# cat ../conf/settings.py > mysite/settings.py;
+	# ln -s /var/site_files/html/index.html $APP_NAME/templates/$APP_NAME/index.html
+	# ln -s /var/site_files/css/ $APP_NAME/static/$APP_NAME/
+	# ln -s /var/site_files/html/ $APP_NAME/templates/$APP_NAME/files;
+	# ln -s /var/site_files/js/ $APP_NAME/static/$APP_NAME/;
+	# ln -s /var/site_files/assets/ $APP_NAME/static/$APP_NAME/;
+	mv ../site_files/html/index.html $APP_NAME/templates/$APP_NAME;
+	mv ../site_files/css/ $APP_NAME/static/$APP_NAME/
+	mv ../site_files/html/ $APP_NAME/templates/$APP_NAME/files;
+	mv ../site_files/js/ $APP_NAME/static/$APP_NAME/js;
+	mv ../site_files/assets/ $APP_NAME/static/$APP_NAME/;
+	# cat ../conf/models.py > $APP_NAME/models.py;
+	# cat ../conf/views.py > $APP_NAME/views.py;
+	# cat ../conf/urls_app.py > $APP_NAME/urls.py;
+	# cat ../conf/urls_project.py > mysite/urls.py;
 
 
 	#TEST SOCKETS
-	cp ../conf/asgi.py mysite/asgi.py
-	cp ../conf/consumers.py $APP_NAME/consumers.py
-	cp ../conf/routing.py $APP_NAME/routing.py
+	# cp ../conf/asgi.py mysite/asgi.py
+	# cp ../conf/consumers.py $APP_NAME/consumers.py
+	# cp ../conf/routing.py $APP_NAME/routing.py
+# 
+	# cp ../conf/managers.py $APP_NAME/managers.py
+# 
+	# echo "python manage.py makemigrations";
+	# python manage.py makemigrations;
+# 
+	# echo "python manage.py migrate";
+	# python manage.py migrate;
 
-	cp ../conf/managers.py $APP_NAME/managers.py
-
-	echo "python manage.py makemigrations";
-	python manage.py makemigrations;
-
-	echo "python manage.py migrate";
-	python manage.py migrate;
-
-	#ADD USER TEST WITH PASSWORD 123456789
-	mkdir -p $APP_NAME/management/commands
-	cp ../conf/createuser.py $APP_NAME/management/commands/createuser.py
-	python manage.py createuser;
+	# ADD USER TEST WITH PASSWORD 123456789
+	# mkdir -p $APP_NAME/management/commands
+	# cp ../conf/createuser.py $APP_NAME/management/commands/createuser.py
+	# python manage.py createuser;
 
 fi
 
-echo "python mysite/manage.py runserver";
-python manage.py runserver $(hostname -i):8080;
+tail -f /dev/null/
+# echo "python mysite/manage.py runserver";
+# python manage.py runserver $(hostname -i):8080;
