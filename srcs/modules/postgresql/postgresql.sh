5ac07fdcce2f757c28ff8d889cc496c32de7482a
fle-tolg <you@example.com>
2023-12-15 14:10:10 +0100

    better script, redirections in log

--- srcs/modules/postgresql/postgresql.sh
+++ srcs/modules/postgresql/postgresql.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/var/db/postgresql.conf" ]; then
 	echo database initialisation;
-	initdb -D /var/db > /dev/null 2>> /var/log/error.log;
+	initdb -D /var/db >> /var/log/postgres.log 2>> /var/log/error.log;
 else
 	echo initialisation already done;
 fi
@@ -11,21 +11,21 @@ export IP_DJANGO=$(ping container_django | head -n 1 | awk '{print $3}'# | cut -
 
 echo "host $DB_NAME all $IP_DJANGO/32 trust" >> /var/db/pg_hba.conf
 
-pg_ctl -D /var/db start > /dev/null 2>> /var/log/error.log;
+pg_ctl -D /var/db start >> /var/log/postgres.log 2>> /var/log/error.log;
 
 if psql -lqt | cut -d \| -f 1 | grep -qw $DB_NAME ; then
 	echo $DB_NAME already created;
 else
 	createdb $DB_NAME;
 
-	psql -d $DB_NAME -c "create user ${DB_USER} with encrypted password '${DB_PASSWORD}';" > /dev/null 2>> /var/log/error.log;
-	psql -d $DB_NAME -c "grant all on SCHEMA public TO ${DB_USER};" > /dev/null 2>> /var/log/error.log;
-	psql -d $DB_NAME -c "grant all privileges on database ${DB_NAME} TO ${DB_USER};" > /dev/null 2>> /var/log/error.log;
+	psql -d $DB_NAME -c "create user ${DB_USER} with encrypted password '${DB_PASSWORD}';" >> /var/log/postgres.log 2>> /var/log/error.log;
+	psql -d $DB_NAME -c "grant all on SCHEMA public TO ${DB_USER};" >> /var/log/postgres.log 2>> /var/log/error.log;
+	psql -d $DB_NAME -c "grant all privileges on database ${DB_NAME} TO ${DB_USER};" >> /var/log/postgres.log 2>> /var/log/error.log;
 
 	echo $DB_NAME created;
 fi
 
-pg_ctl -D /var/db stop > /dev/null 2>> /var/log/error.log;
+pg_ctl -D /var/db stop >> /var/log/postgres.log 2>> /var/log/error.log;
 
 echo $DB_NAME is launching...;
 
