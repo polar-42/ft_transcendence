ceeaac894852009b870710a9ee1df3e4e4a80d43
mjuin <mickael.juin.16@gmail.com>
2023-12-24 15:49:16 +0100

    Update Tournament app to have a more fluid url nav

--- srcs/modules/django/conf/ft_transcendence/tournamentsApp/urls.py
+++ srcs/modules/django/conf/ft_transcendence/tournamentsApp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('tournamentsHome/', views.Home_view),
-	path('tournamentsCreation/', views.tournaments_creation),
+	path('Home/', views.Home_view),
+	path('Create/', views.tournaments_creation),
 	path('create_tournaments/', views.create_tournaments),
 	path('get_tournaments/', views.get_tournaments),
-	path('tournamentsJoin/', views.view_JoinPage),
+	path('Join/', views.view_JoinPage),
 	path('get_tournaments_html/', views.get_tournaments_html),
 	path('join_tournaments/', views.join_tournaments),
 	path('tournament/', views.Tournament_view),
