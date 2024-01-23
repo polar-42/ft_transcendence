c1439536ce9438d8711d1a81bf5840aaddc2d0e1
mjuin <mickael.juin.16@gmail.com>
2024-01-23 14:45:50 +0100

    Start 2fa

--- srcs/modules/django/conf/ft_transcendence/authApp/models.py
+++ srcs/modules/django/conf/ft_transcendence/authApp/models.py
@@ -11,3 +11,5 @@ class User(AbstractUser):
     blockedUser = ArrayField(models.CharField(), blank=True, null=True)
     allPrivateTalks = ArrayField(models.CharField(), blank=True, null=True)
     avatarImage = models.BinaryField(blank=True, default=None, null=True)
+    tfEnable = models.BooleanField(default=False)
+    tfType = models.IntegerField(default=0)
---
+++ srcs/modules/django/conf/ft_transcendence/ft_transcendence/globalEnum.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+class TF_Type(IntEnum):
+    Disable = -1
+    Email = 1
+    Auth = 2
+    Phone = 3
