08f0cc812f33325ac008ae5732a4b1f6b4ac1c61
fle-tolg <you@example.com>
2024-02-19 16:48:43 +0100

    AI working

--- srcs/modules/django/conf/ft_transcendence/pongGameApp/IA/PongIA.py
+++ srcs/modules/django/conf/ft_transcendence/pongGameApp/IA/PongIA.py
@@ -13,13 +13,19 @@ class pongGameIA(threading.Thread):
 
 			if self.isRunning is True:
 
-				if self.ball_pos_y < self.ia_pos_y - 0.9:
+				if self.ball_pos_y < self.ia_pos_y - 0.7:
 					self.gameThread.inputGame('ArrowDown', 1)
-				elif self.ball_pos_y > self.ia_pos_y + 0.9:
+					if self.ia_pos_y - 0.2 < 2.7:
+						self.ia_pos_y -= 0.2
+				elif self.ball_pos_y > self.ia_pos_y + 0.7:
 					self.gameThread.inputGame('ArrowUp', 1)
+					if self.ia_pos_y + 0.2 > -2.7:
+						self.ia_pos_y += 0.2
 				else:
 					self.gameThread.inputGame('StopMovementDown', 1)
 					self.gameThread.inputGame('StopMovementUp', 1)
+
+				print('self.ia_pos_y =', self.ia_pos_y, 'self.ball_pos_y =', self.ball_pos_y)
 
 			time.sleep(0.03)
 
@@ -35,12 +41,25 @@ class pongGameIA(threading.Thread):
 		self.ball_pos_x = 0
 		self.ball_pos_y = 0
 		self.ia_pos_y = 0
-		self.ia_pos_x = 4.
+		self.ia_pos_x = 4
+		self.ball_speed = 0
 
 	def receiveDataFromGameIA(self, game, isRunning):
 		self.isRunning = isRunning
-		gameState = game.get_ball()
+		ball = game.get_ball()
 
-		ball_pos_x, self.ball_pos_y = gameState.get_pos()
+		self.ball_pos_x, self.ball_pos_y = ball.get_pos()
+		self.ball_dx, self.ball_dy = ball.get_direction()
+		self.ball_speed = ball.get_speed()
+
+		i = 0
+		while i < 30:
+			if not(3.8 - 0.15 >= self.ball_pos_y + self.ball_dy * self.ball_speed >= -3.8 + 0.15):
+				self.ball_dy *= -1
+
+			self.ball_pos_y += self.ball_dy * self.ball_speed
+			self.ball_pos_x += self.ball_dx * self.ball_speed
+			i += 1
+
 		player, ia = game.get_players()
 		ia_pos_x, self.ia_pos_y = ia.get_pos()
